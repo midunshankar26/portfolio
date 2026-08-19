ed7683373d47b5cba53d305ed1f579d5b400ee51
midunshankar26 <midunshankar.2aj24@kprcas.ac.in>
2026-08-19 12:11:54 +0530

    Implement dark mode toggle with local storage
    Refactor theme toggle functionality to support dark mode with local storage.

--- script.js
+++ script.js
@@ -2,17 +2,46 @@ window.addEventListener("load", () => {
     document.body.style.opacity = "1";
 });
 
-console.log("Portfolio Website Loaded Successfully");
-const toggleBtn = document.getElementById("theme-toggle");
+const themeToggle =
+document.getElementById("themeToggle");
 
-toggleBtn.addEventListener("click", () => {
+// Load Saved Theme
 
-    document.body.classList.toggle("light-mode");
+if(localStorage.getItem("theme") === "dark"){
 
-    if(document.body.classList.contains("light-mode")){
-        toggleBtn.textContent = "☀️";
+    document.body.classList.add("dark-mode");
+
+    themeToggle.innerHTML =
+    "☀️ Light Mode";
+
+}
+
+// Toggle Theme
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        themeToggle.innerHTML =
+        "☀️ Light Mode";
+
+        localStorage.setItem(
+            "theme",
+            "dark"
+        );
+
     }else{
-        toggleBtn.textContent = "🌙";
+
+        themeToggle.innerHTML =
+        "🌙 Dark Mode";
+
+        localStorage.setItem(
+            "theme",
+            "light"
+        );
+
     }
 
 });
